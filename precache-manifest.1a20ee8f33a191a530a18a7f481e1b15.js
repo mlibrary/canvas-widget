3997093de15696be075c9ae1530b400cab9d2f53
Jon Earley <jonathan.a.earley@gmail.com>
2018-11-06 14:01:29 -0500

    Updates

---
+++ precache-manifest.1a20ee8f33a191a530a18a7f481e1b15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4c82cc6c39f1ae252b9",
+    "url": "/canvas-widget/static/js/runtime~main.f4c82cc6.js"
+  },
+  {
+    "revision": "060802dfd28b7584b6f4",
+    "url": "/canvas-widget/static/js/main.060802df.chunk.js"
+  },
+  {
+    "revision": "c2b16fc4972c451cb148",
+    "url": "/canvas-widget/static/js/1.c2b16fc4.chunk.js"
+  },
+  {
+    "revision": "060802dfd28b7584b6f4",
+    "url": "/canvas-widget/static/css/main.5a2f29a2.chunk.css"
+  },
+  {
+    "revision": "566455fe88910058f2b89b7327a8f063",
+    "url": "/canvas-widget/index.html"
+  }
+];
